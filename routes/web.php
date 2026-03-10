c396ee6ba86a4de69e2e56f67cbd11265f6f53bf
AlexD1305 <137122395+AlexD1305@users.noreply.github.com>
2026-03-10 16:46:10 -0600

    Modulo de administracion de usuarios funcionando

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,33 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-
+// Redirigir al login al entrar a /
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
+// Rutas de autenticación
 Route::get('/login', [AuthController::class, 'mostrarLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/registro', [AuthController::class, 'mostrarRegistro'])->name('register');
 Route::post('/registro', [AuthController::class, 'registro']);
+// funcion de inactivar
+Route::post('/users/{id}/inactivate', [App\Http\Controllers\UserController::class, 'inactivate'])->name('users.inactivate');
 
-
+// Grupo de rutas protegidas por autenticación
 Route::middleware('auth')->group(function () {
+
+    // Página de bienvenida
     Route::get('/pagina', function () {
         return view('pagina');
     })->name('pagina');
-    
+
+    // Logout
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-});
+
+    // Rutas de administración de usuarios
+    Route::resource('users', UserController::class);
+}); 
